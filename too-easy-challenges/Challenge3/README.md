73f5edc1937c82e4fafb1dc58e163661e8293e9d
Ho Hieu Luc <kingofika123@gmail.com>
2022-07-12 10:15:39 +0700

    add Challenge3

---
+++ too-easy-challenges/Challenge3/README.md
@@ -0,0 +1,15 @@
+# Pig Game
+##### A simple game for learning DOM manipulation and events
+
+
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he wishes. Each result get added to his ROUND       score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After     that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+## Change [Pig Game]() to follow this rule:
+
+A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
---
+++ too-easy-challenges/Challenge3/app.js
@@ -0,0 +1,115 @@
+const parseNumber = (value, defaultValue = 0) => {
+    if (typeof value !== 'string') {
+        return defaultValue;
+    }
+    const number = parseInt(value);
+    return isNaN(number) ? defaultValue : number;
+};
+
+const createPlayer = (id) => {
+    const globalScoreDOM = document.querySelector(`#score-${id}`);
+    const roundScoreDOM = document.querySelector(`#current-${id}`);
+
+    return {
+        DOM: {
+            panel: document.querySelector(`.player-${id}-panel`),
+            name: document.querySelector(`#name-${id}`),
+            globalScore: globalScoreDOM,
+            roundScore: roundScoreDOM,
+        },
+        stats: {
+            previousDice: 0,
+            globalScore: parseNumber(globalScoreDOM.textContent, 0),
+            roundScore: parseNumber(roundScoreDOM.textContent, 0)
+        }
+    }
+};
+
+const renderScore = () => {
+    player1.DOM.globalScore.textContent = player1.stats.globalScore;
+    player1.DOM.roundScore.textContent = player1.stats.roundScore;
+    player2.DOM.globalScore.textContent = player2.stats.globalScore;
+    player2.DOM.roundScore.textContent = player2.stats.roundScore;
+}
+
+const player1 = createPlayer(0);
+const player2 = createPlayer(1);
+
+let currentPlayer = player1;
+
+const startGame = () => {
+    player1.DOM.panel.classList.remove('winner');
+    player2.DOM.panel.classList.remove('winner');
+
+    player1.DOM.panel.classList.add('active');
+    player2.DOM.panel.classList.remove('active');
+
+    player1.stats.globalScore = 0;
+    player1.stats.roundScore = 0;
+    player2.stats.globalScore = 0;
+    player2.stats.roundScore = 0;
+
+    renderScore();
+
+    rollDiceButton.classList.remove('disabled');
+    holdButton.classList.remove('disabled');
+    
+    rollDiceButton.disabled = false;
+    holdButton.disabled = false;
+
+    currentPlayer = player1;
+};
+
+const newGameButton = document.querySelector('.btn-new');
+newGameButton.addEventListener('click', startGame);
+
+const diceDOM = document.querySelector('.dice');
+
+const rollDice = () => {
+    const dice = Math.floor(Math.random() * 6) + 1;
+    diceDOM.src = `dice-${dice}.png`;
+    return dice;
+};
+
+const nextPlayer = () => {
+    currentPlayer.DOM.panel.classList.remove('active');
+    if (currentPlayer === player1) {
+        currentPlayer = player2;
+    } else {
+        currentPlayer = player1;
+    }
+    currentPlayer.DOM.panel.classList.add('active');
+};
+
+const rollDiceButton = document.querySelector('.btn-roll');
+rollDiceButton.addEventListener('click', () => {
+    const dice = rollDice();
+    if (dice !== 1 && !(currentPlayer.stats.previousDice === 6 && dice === 6)) {
+        currentPlayer.stats.roundScore += dice;
+        currentPlayer.stats.previousDice = dice;
+    } else {
+        currentPlayer.stats.roundScore = 0;
+        currentPlayer.stats.previousDice = 0;
+        nextPlayer();
+    }
+    renderScore();
+});
+
+const holdButton = document.querySelector('.btn-hold');
+holdButton.addEventListener('click', () => {
+    currentPlayer.stats.globalScore += currentPlayer.stats.roundScore;
+    currentPlayer.stats.roundScore = 0;
+
+    if (currentPlayer.stats.globalScore >= 100) {
+        currentPlayer.DOM.panel.classList.add('winner');
+        currentPlayer.DOM.panel.classList.remove('active');
+        rollDiceButton.classList.add('disabled');
+        holdButton.classList.add('disabled');
+
+        rollDiceButton.disabled = true;
+        holdButton.disabled = true;
+    } else {
+        nextPlayer();
+    }
+    renderScore();
+});
