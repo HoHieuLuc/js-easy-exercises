e7876ef2ad4520d73d7270c05ecc3aba8ee0b69d
Ho Hieu Luc <kingofika123@gmail.com>
2022-07-12 11:52:12 +0700

    add fail-pass-averages

---
+++ assignment-easy-2/fail-pass-averages.js
@@ -0,0 +1,22 @@
+const failPassAverages = (array = []) => {
+    const faillingMarks = array.filter(value => value < 50);
+    const passingMarks = array.filter(value => value >= 50);
+
+    const averageFailMark = faillingMarks.reduce(
+        (sum, curr) => sum + curr, 0
+    ) / faillingMarks.length;
+
+    const averagePassMark = passingMarks.reduce(
+        (sum, curr) => sum + curr, 0
+    ) / passingMarks.length;
+
+    console.log([
+        isNaN(averageFailMark) ? -1 : Math.round(averageFailMark),
+        isNaN(averagePassMark) ? -1 : Math.round(averagePassMark)
+    ]);
+
+};
+
+failPassAverages([63, 65, 33]);
+failPassAverages([63, 62, 100, 100]);
+failPassAverages([33, 42, 20, 10]);
