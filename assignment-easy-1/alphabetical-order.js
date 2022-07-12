c925217b1e0b64c7300e45aaa53d9f1b94b33fed
Ho Hieu Luc <kingofika123@gmail.com>
2022-07-12 11:11:19 +0700

    update sort function

--- assignment-easy-1/alphabetical-order.js
+++ assignment-easy-1/alphabetical-order.js
@@ -1,7 +1,12 @@
 const _ten = (string = '') => {
     const str = string.split('');
     const result = str.sort((prev, curr) => {
-        return prev.toLocaleLowerCase() > curr.toLocaleLowerCase() ? 1 : -1;
+        if (prev.toLocaleLowerCase() > curr.toLocaleLowerCase()) {
+            return 1;
+        } else if (prev.toLocaleLowerCase() < curr.toLocaleLowerCase()) {
+            return -1;
+        }
+        return 0;
     }).join('');
 
     console.log(`The string converted in alphabetical order is: ${result}`);
