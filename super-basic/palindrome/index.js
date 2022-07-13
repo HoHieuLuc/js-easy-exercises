f1a007461a44a0d3c431b22bbf0fbcf8c90e4736
Ho Hieu Luc <kingofika123@gmail.com>
2022-07-13 15:52:17 +0700

    finish palindrome

--- super-basic/palindrome/index.js
+++ super-basic/palindrome/index.js
@@ -7,8 +7,18 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {
-  
+function palindrome(str = '') {
+    let leftString = '';
+    let rightString = '';
+    if (str.length % 2 === 0) {
+        leftString = str.slice(0, str.length / 2);
+        rightString = str.slice(str.length / 2, str.length);
+    } else {
+        leftString = str.slice(0, Math.floor(str.length / 2));
+        rightString = str.slice(Math.floor(str.length / 2) + 1, str.length);
+    }
+
+    return leftString === rightString.split('').reverse().join('');
 }
 
 module.exports = palindrome;
