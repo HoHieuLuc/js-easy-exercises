2ee8efa33d76eaf3bb08eeadfc239e1727fbb8d0
Ho Hieu Luc <kingofika123@gmail.com>
2022-07-13 14:34:44 +0700

    finish levelwidth

--- super-basic/levelwidth/index.js
+++ super-basic/levelwidth/index.js
@@ -12,8 +12,22 @@
 // Answer: [1, 3, 2]
 
 function levelWidth(root) {
+    const arr = [];
 
-  return counters;
+    function traverse(node, level) {
+        if (arr[level]) {
+            arr[level]++;
+        } else {
+            arr[level] = 1;
+        }
+
+        if (node.children.length > 0) {
+            node.children.forEach(child => traverse(child, level + 1));
+        }
+    }
+
+    traverse(root, 0);
+    return arr;
 }
 
 module.exports = levelWidth;
