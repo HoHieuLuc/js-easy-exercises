c75829f1cba08204ee4a37c306db76287c09fd1f
Ho Hieu Luc <kingofika123@gmail.com>
2022-07-13 14:10:51 +0700

    finish fib

--- super-basic/fib/index.js
+++ super-basic/fib/index.js
@@ -9,11 +9,27 @@
 //   fib(4) === 3
 
 function memoize(fn) {
-  
+    const cache = {};
+
+    return (...args) => {
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result;
+    }
+
 }
 
 function slowFib(n) {
- 
+    if (n < 2) {
+        return n;
+    }
+
+    return slowFib(n - 1) + slowFib(n - 2);
 }
 
 const fib = memoize(slowFib);
