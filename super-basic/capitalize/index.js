e96c5b4a018c3ba0e71d3074ecde50c460b2c57e
Ho Hieu Luc <kingofika123@gmail.com>
2022-07-13 13:27:46 +0700

    finish capitalize

--- super-basic/capitalize/index.js
+++ super-basic/capitalize/index.js
@@ -7,9 +7,10 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {
-
-  return result;
+function capitalize(str = '') {
+    return str.split(' ').map((string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }).join(' ');
 }
 
 module.exports = capitalize;
