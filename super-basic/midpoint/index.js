0fce38fca252074f8c8443b639c8104816cc386d
Ho Hieu Luc <kingofika123@gmail.com>
2022-07-13 15:46:44 +0700

    finish midpoint

--- super-basic/midpoint/index.js
+++ super-basic/midpoint/index.js
@@ -13,7 +13,13 @@
 //   midpoint(l); // returns { data: 'b' }
 
 function midpoint(list) {
-  
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
 }
 
 module.exports = midpoint;
