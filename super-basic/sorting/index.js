70e2f5b014e39241314e3840f0d2aea6fbd4a18b
Ho Hieu Luc <kingofika123@gmail.com>
2022-07-13 16:46:25 +0700

    finish sorting

--- super-basic/sorting/index.js
+++ super-basic/sorting/index.js
@@ -1,19 +1,56 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
-function bubbleSort(arr) {
-  
+function bubbleSort(arr = []) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
 }
 
-function selectionSort(arr) {
+function selectionSort(arr = []) {
+    for (let i = 0; i < arr.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        if (min !== i) {
+            let temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+    return arr;
 }
 
-function mergeSort(arr) {
-  
+function mergeSort(arr = []) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, right);
 }
 
-function merge(left, right) {
-  
+function merge(left = [], right = []) {
+    let result = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+    return result.concat(left, right);
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
--- super-basic/sorting/test.js
+++ super-basic/sorting/test.js
@@ -5,34 +5,34 @@ const mergeSort = S.mergeSort;
 const merge = S.merge;
 
 function getArray() {
-  return [100, -40, 500, -124, 0, 21, 7];
+    return [100, -40, 500, -124, 0, 21, 7];
 }
 
 function getSortedArray() {
-  return [-124, -40, 0, 7, 21, 100, 500];
+    return [-124, -40, 0, 7, 21, 100, 500];
 }
 
 describe('Bubble sort', () => {
-  test('sorts an array', () => {
-    expect(bubbleSort(getArray())).toEqual(getSortedArray());
-  });
+    test('sorts an array', () => {
+        expect(bubbleSort(getArray())).toEqual(getSortedArray());
+    });
 });
 
 describe('Selection sort', () => {
-  test('sorts an array', () => {
-    expect(selectionSort(getArray())).toEqual(getSortedArray());
-  });
+    test('sorts an array', () => {
+        expect(selectionSort(getArray())).toEqual(getSortedArray());
+    });
 });
 
 describe('Merge sort', () => {
-  test('merge function can join together two sorted arrays', () => {
-    const left = [1, 10];
-    const right = [2, 8, 12];
+    test('merge function can join together two sorted arrays', () => {
+        const left = [1, 10];
+        const right = [2, 8, 12];
 
-    expect(merge(left, right)).toEqual([1,2,8,10,12]);
-  });
+        expect(merge(left, right)).toEqual([1, 2, 8, 10, 12]);
+    });
 
-  test('sorts an array', () => {
-    expect(mergeSort(getArray())).toEqual(getSortedArray());
-  });
+    test('sorts an array', () => {
+        expect(mergeSort(getArray())).toEqual(getSortedArray());
+    });
 });
