ae0cdaf1eb1fc19677448ed23a457225c8aba43b
Ho Hieu Luc <kingofika123@gmail.com>
2022-07-13 15:44:30 +0700

    finish maxchar

--- super-basic/maxchar/index.js
+++ super-basic/maxchar/index.js
@@ -5,8 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {
-  
+function maxChar(str = '') {
+    const charMap = {};
+    let max = 0;
+
+    for (let char of str) {
+        if (charMap[char]) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+    }
+
+    return Object.entries(charMap).reduce((prevChar, [char, value]) => {
+        if (value > max) {
+            max = value;
+            return char;
+        }
+        return prevChar;
+    }, '');
 }
 
 module.exports = maxChar;
